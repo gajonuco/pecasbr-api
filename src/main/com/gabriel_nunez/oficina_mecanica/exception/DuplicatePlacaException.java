a7a7afe5b90faef24a25c3dec31e1db3c42a1fd0
Gabriel <gajonuco@gmail.com>
2025-04-13 11:17:07 -0300

    Adicionado a entidade Veiculo no projeto MVC

---
+++ src/main/com/gabriel_nunez/oficina_mecanica/exception/DuplicatePlacaException.java
@@ -0,0 +1,7 @@
+package com.gabriel_nunez.oficina_mecanica.exception;
+
+public class DuplicatePlacaException extends RuntimeException {
+    public DuplicatePlacaException(String message) {
+        super(message);
+    }
+}
