0bd03f50a4577a08bca83a734bed6fbefe22b82c
gajonuco <gajonuco@gmail.com>
2026-09-10 06:06:18 -0300

    fix(cliente): corrigir nome do método findByClienteId em EnderecoDAO (#7)

--- src/main/java/com/gajonuco/pecasbr/dao/EnderecoDAO.java
+++ src/main/java/com/gajonuco/pecasbr/dao/EnderecoDAO.java
@@ -6,5 +6,5 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface EnderecoDAO extends CrudRepository<Endereco,Integer> {
-    List<Endereco> findByClientId(int clienteId);
+    List<Endereco> findByClienteId(int clienteId);
 }
